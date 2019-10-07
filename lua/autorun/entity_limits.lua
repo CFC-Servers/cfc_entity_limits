1918e5b0d62cc78dfbb0fa9e5fb1ff5e690678d0
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-07 17:57:21 -0400

    Various whitespace fixes

--- lua/autorun/entity_limits.lua
+++ lua/autorun/entity_limits.lua
@@ -3,15 +3,15 @@ local limits = {
 }
 
 local function getLimitName( class )
-    return "cfc_limits_"..class
+    return "cfc_limits_" .. class
 end
 
 for class, limit in pairs( limits ) do
     local limitName = getLimitName( class )
-    CreateConVar( "sbox_max"..limitName , tostring(limit))
-    
+    CreateConVar( "sbox_max" .. limitName, tostring( limit ) )
+
     if CLIENT then
-        language.Add("sboxlimit_"..limitName, "You've hit the "..class.." limit!")
+        language.Add( "sboxlimit_" .. limitName, "You've hit the " .. class .. " limit!" )
     end
 end
 
@@ -19,7 +19,7 @@ if not SERVER then return end
 
 local function playerCanSpawn( ply, class )
     if not limits[class] then return end
-    
+
     local canSpawn = ply:CheckLimit( getLimitName( class ) )
     if not canSpawn then return false end
 end
@@ -27,12 +27,12 @@ end
 local function playerSpawnedEnt( ply, ent )
     local class = ent:GetClass()
     if not limits[class] then return end
-    
+
     ply:AddCount( getLimitName( class ), ent )
 end
 
 hook.Remove( "PlayerSpawnSENT", "CFC_EntLimits_CanPlayerSpawn" )
-hook.Add( "PlayerSpawnSENT", "CFC_EntLimits_CanPlayerSpawn", playerCanSpawn)
+hook.Add( "PlayerSpawnSENT", "CFC_EntLimits_CanPlayerSpawn", playerCanSpawn )
 
-hook.Remove("PlayerSpawnedSENT", "CFC_EntLimits_PlayerSpawnedEnt")
-hook.Add( "PlayerSpawnedSENT", "CFC_EntLimits_PlayerSpawnedEnt", playerSpawnedEnt)
+hook.Remove( "PlayerSpawnedSENT", "CFC_EntLimits_PlayerSpawnedEnt" )
+hook.Add( "PlayerSpawnedSENT", "CFC_EntLimits_PlayerSpawnedEnt", playerSpawnedEnt )
